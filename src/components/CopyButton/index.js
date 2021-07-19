0093358a3eecb23a3d50e765082ec084f86448be
Fernanda Nardoni <thefray.br@gmail.com>
2021-07-19 16:33:31 -0300

    fixed button problems

--- src/components/CopyButton/index.js
+++ src/components/CopyButton/index.js
@@ -6,6 +6,11 @@ export function CopyButton(props) {
     const [copyStatus, setCopyStatus] = useState('Copy to clipboard')
 
     return (
-        <button onClick={props.event, () => setCopyStatus('Copied')} onMouseLeave={setCopyStatus('Copy to clipboard')}>{copyStatus}</button>
+            <button 
+            onClick={() => {
+                navigator.clipboard.writeText(props.content)
+                setCopyStatus('Copied')
+            }}
+            >{copyStatus}</button>
     )
 }
--- src/components/EmojiList/index.js
+++ src/components/EmojiList/index.js
@@ -5,21 +5,25 @@ import { useState } from 'react';
 import './style.css';
 
 export function EmojiList() {
-   const [search, setSearch] = useState("");
-
+    const [search, setSearch] = useState("");
     
+
+
     return (
         <>
-            <Search value={search} event={(e) => setSearch(e.target.value)}/>
+            <Search value={search} event={(e) => setSearch(e.target.value)} />
             <ul>
-            {arrayEmoji
-            .filter(emoji => emoji.keywords.includes(search))
-            .map(emoji => 
-                <li>{emoji.symbol + emoji.title}
-                <CopyButton event={() => navigator.clipboard.writeText(emoji.symbol)}/>
-                </li>
-            )}    
-            </ul>               
+                {arrayEmoji
+                    .filter(emoji => emoji.keywords.includes(search))
+                    .map(emoji =>
+
+                        <li>
+                            {emoji.symbol + emoji.title}
+                            <CopyButton content={emoji.symbol}/>
+                        </li>
+
+                    )}
+            </ul>
         </>
     )
 }
